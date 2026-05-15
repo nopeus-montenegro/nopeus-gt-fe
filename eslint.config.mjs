328e9b7d931340a08ebf72a033e8604951842159
Iurii Pototskii <nopeus.montenegro@gmail.com>
2026-05-15 19:05:20 +0200

    init: - next - ts - tanstack - zustand - prisma - tailwind - shadcn - lucide

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import stylistic from '@stylistic/eslint-plugin';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: true,
+    jsx: true,
+  }),
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    '/prisma',
+    '/src/generated/prisma',
+  ]),
+]);
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-600 text-slate-50 antialiased">
+      <div className="relative flex flex-col items-center gap-6 text-center">
+        <div className="absolute -top-24 -z-10 h-64 w-64 bg-blue-600/20 blur-[120px]" />
+
+        <h1 className="text-6xl font-black tracking-tighter uppercase italic">
+          Nopeus
+          {' '}
+          <span className="text-blue-500 not-italic">GT</span>
+        </h1>
+
+        <p className="max-w-125 text-lg text-slate-400">
+          Professional lap timing and telemetry for your
+          <span className="text-slate-200"> Thrustmaster T598</span>
+          {' '}
+          setup.
+        </p>
+
+        <div className="flex gap-4 mt-4">
+          <Button variant="default" size="lg">
+            START SESSION
+          </Button>
+          <Button variant="outline" size="lg">
+            DASHBOARD
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
+}
